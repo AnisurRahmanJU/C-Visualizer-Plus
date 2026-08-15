ee041d1fa594148a24d2d48ae9a926ea455ae9f6
Md. Anisur Rahman <poetanis@gmail.com>
2026-08-15 12:51:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2269,7 +2269,7 @@ class CInterpreter {
         const newDecl = `${newBase} ${d.name}${newSizeM ? `[${newSizeM[1]}]` : ''};`;
         const prevDecl = `${prevBase} ${d.name}${prevSizeM ? `[${prevSizeM[1]}]` : ''};`;
         throw new Error(
-          `/user-input:${s.ln}:1: error: redefinition of '${d.name}' with a datatype: ` +
+          `/user-input:${s.ln}:1: error: redefinition of '${d.name}' with a different datatype: ` +
           `'${typeStr}' vs '${prev.type}'<br>` +
           `&nbsp;&nbsp;&nbsp;&nbsp;${newDecl}<br>` +
           `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;^<br>` +
