0e13c8bc247760bcdfdb8081cb3bb499fea14231
Md. Anisur Rahman <poetanis@gmail.com>
2026-08-21 16:45:24 +0600

    Update app.js

--- app.js
+++ app.js
@@ -226,6 +226,15 @@ int main() {
     return 0;
 }`,
 for_loop:`#include <stdio.h>
+
+int main() {
+    int i;
+    for(i = 1; i<= 10; i = i + 1){
+        printf("%d\\n", i);
+    }
+    return 0;
+}`,
+nested_for_loop:`#include <stdio.h>
 int main() 
 {
     int i;
