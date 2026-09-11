1076c5f6f3b979618d1d15ae413ed5295e3b1598
Md. Anisur Rahman <poetanis@gmail.com>
2026-09-11 16:51:29 +0600

    Change loop condition in main function

--- backend/app.js
+++ backend/app.js
@@ -593,7 +593,7 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 int main() {
-    for (int i = 0; i <= 5; i++) {
+    for (int i = 0; i < 10; i++) {
         if (i == 0) {
             printf("fib(0) = 0\\n");
         } else if (i == 1) {
